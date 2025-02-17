02248b5010a3a64f8815289e0169fc2e142a0970
SohamSarkar7 <154735000+SohamSarkar7@users.noreply.github.com>
2025-02-17 15:22:04 +0530

    ETL created

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -12,9 +12,9 @@ class NetworkSecurityException(Exception):
         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(self.file_name,self.lineno,str(self.error_message))
+        return "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(self.file_name,self.lineno,str(self.error_message))
 
-        return error_message
+        
 
 # if __name__ == '__main__':
 #     try:
